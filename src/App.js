a7c8852848dc7a6c0a9b336e7086daced8fef0b9
Ian A <iastesana@gmail.com>
2020-11-18 16:02:12 -0800

    completed file structure created a route, switch, and started the AboutMe page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+import AboutMe from "./page/AboutMe"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/* this is wherer you put a nav bar */}
+        <Switch>  {/* you can only have 1 switch and only 1 route can be active at any given time.*/}
+          <Route exact path="/" component={AboutMe} />
+        </Switch>
+        {/* thi sis wherer you put the footer. */}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/page/AboutMe.js
@@ -0,0 +1,10 @@
+function AboutMe() {
+	return (
+		// divs ClassName there has to be a wrapper, or element that contains it all
+		<div>
+			<h1>About me start</h1>
+		</div>
+	)
+}
+
+export default AboutMe;
